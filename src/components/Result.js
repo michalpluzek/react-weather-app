3afa7fe893cf5117e94c1bcbff13085058ae4566
Michał Płużek <michalpluzek@gmail.com>
2021-11-24 10:46:30 +0100

    Add template to show data

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,10 +2,46 @@ import React from "react";
 import "./Result.css";
 
 const Result = (props) => {
-  const error = <h4>Błąd</h4>;
-  const result = <div>Pogoda miasto...</div>;
+  const { date, city, sunrise, sunset, temp, wind, pressure } = props.weather;
 
-  return <div>{props.error ? error : result}</div>;
+  const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString();
+  const sunsetTime = new Date(sunset * 1000).toLocaleTimeString();
+  const error = (
+    <p>
+      Nie ma takiego miasta w bazie: <strong>{city}</strong>
+    </p>
+  );
+
+  let result = "";
+  if (!props.error && city) {
+    result = (
+      <>
+        <p>
+          Pogoda dla: <strong>{city}</strong>
+        </p>
+        <p>
+          Aktualny czas: <strong>{date}</strong>
+        </p>
+        <p>
+          Temperatura: <strong>{temp} &#8451;</strong>
+        </p>
+        <p>
+          Wschód słońca o <strong>{sunriseTime}</strong>
+        </p>
+        <p>
+          Zachód słońca o <strong>{sunsetTime}</strong>
+        </p>
+        <p>
+          Siła wiatru: <strong>{wind} m/s</strong>
+        </p>
+        <p>
+          Ciśnienie: <strong>{pressure} hPa</strong>
+        </p>
+      </>
+    );
+  }
+
+  return <div className="result">{props.error ? error : result}</div>;
 };
 
 export default Result;
