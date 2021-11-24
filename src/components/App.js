7442bbdde70ec1fee03586ee5baf5cf8dc8a12c6
Michał Płużek <michalpluzek@gmail.com>
2021-11-24 10:07:03 +0100

    Add save data to state and error

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Form from "./Form";
 import Result from "./Result";
 
+const APIKey = "97e45c83e89d2245ef87c28399a60b80";
+
 class App extends React.Component {
   state = {
     inputValue: "",
@@ -15,7 +17,7 @@ class App extends React.Component {
       wind: "",
       pressure: "",
     },
-    err: "",
+    err: false,
   };
 
   handleInputChange = (e) => {
@@ -26,7 +28,7 @@ class App extends React.Component {
 
   handleCitySubmit = (e) => {
     e.preventDefault();
-    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.inputValue}&appid=97e45c83e89d2245ef87c28399a60b80`;
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.inputValue}&appid=${APIKey}`;
 
     fetch(API)
       .then((response) => {
@@ -37,20 +39,40 @@ class App extends React.Component {
         }
       })
       .then((data) => {
-        console.log(data);
+        const actualTime = new Date().toLocalString();
+
+        this.setState({
+          weather: {
+            date: actualTime,
+            city: this.state.inputValue,
+            sunrise: data.sys.sunrise.toLocalTimeString(),
+            sunset: data.sys.sunset.toLocalTimeString(),
+            temp: data.main.temp,
+            wind: data.wind.speed,
+            pressure: data.main.pressure,
+          },
+          err: false,
+        });
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        this.setState({
+          err: true,
+        });
+      });
   };
 
   render() {
+    const { err, inputValue } = this.state;
+
     return (
       <div className="App">
         <Form
-          value={this.state.inputValue}
+          value={inputValue}
           change={this.handleInputChange}
           submit={this.handleCitySubmit}
         />
-        <Result />
+        <Result error={err} />
       </div>
     );
   }
--- src/components/Result.js
+++ src/components/Result.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./Result.css";
 
 const Result = (props) => {
-  return <div>Result</div>;
+  const error = <h4>Błąd</h4>;
+  const result = <div>Pogoda miasto...</div>;
+
+  return <div>{props.error ? error : result}</div>;
 };
 
 export default Result;
