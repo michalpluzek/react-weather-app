f84c0ec6ef0cb8cb69e358ea3fc02c450d561d96
Michał Płużek <michalpluzek@gmail.com>
2021-12-01 15:15:47 +0100

    Refactoring to hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,83 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Form from "./Form";
 import Result from "./Result";
 
 const APIKey = "97e45c83e89d2245ef87c28399a60b80";
 
-class App extends React.Component {
-  state = {
-    inputValue: "",
-    weather: {
-      date: "",
-      city: "",
-      sunrise: "",
-      sunset: "",
-      temp: "",
-      wind: "",
-      pressure: "",
-    },
-    err: false,
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [weather, setWeather] = useState({
+    date: "",
+    city: "",
+    sunrise: "",
+    sunset: "",
+    temp: "",
+    wind: "",
+    pressure: "",
+  });
+  const [err, setErr] = useState(false);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
   };
 
-  handleInputChange = (e) => {
-    this.setState({
-      inputValue: e.target.value,
-    });
-  };
+  useEffect(() => {
+    if (inputValue.length === 0) return;
 
-  componentDidUpdate = (prevProps, prevState) => {
-    if (this.state.inputValue.length === 0) return;
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${APIKey}&units=metric`;
 
-    if (prevState.inputValue !== this.state.inputValue) {
-      const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.inputValue}&appid=${APIKey}&units=metric`;
+    fetch(API)
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw Error(" Nie udało się");
+        }
+      })
+      .then((data) => {
+        const actualTime = new Date().toLocaleString();
 
-      fetch(API)
-        .then((response) => {
-          if (response.status === 200) {
-            return response.json();
-          } else {
-            throw Error(" Nie udało się");
-          }
-        })
-        .then((data) => {
-          const actualTime = new Date().toLocaleString();
+        setErr(false);
+        setWeather(() => ({
+          date: actualTime,
+          city: inputValue,
+          sunrise: data.sys.sunrise,
+          sunset: data.sys.sunset,
+          temp: data.main.temp,
+          wind: data.wind.speed,
+          pressure: data.main.pressure,
+        }));
+      })
+      .catch((err) => {
+        console.log(err);
+        setErr(true);
+        setWeather((prevValue) => ({
+          ...prevValue,
+          city: inputValue,
+        }));
+      });
+  }, [inputValue]);
 
-          this.setState((prevState) => ({
-            weather: {
-              date: actualTime,
-              city: prevState.inputValue,
-              sunrise: data.sys.sunrise,
-              sunset: data.sys.sunset,
-              temp: data.main.temp,
-              wind: data.wind.speed,
-              pressure: data.main.pressure,
-            },
-            err: false,
-          }));
-        })
-        .catch((err) => {
-          console.log(err);
-          this.setState((prevState) => ({
-            err: true,
-            weather: {
-              city: prevState.inputValue,
-            },
-          }));
-        });
-    }
-  };
-
-  render() {
-    const { err, inputValue, weather } = this.state;
-
-    return (
-      <div className="app">
-        <Form value={inputValue} change={this.handleInputChange} />
-        <Result error={err} weather={weather} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="app">
+      <Form value={inputValue} change={handleInputChange} />
+      <Result error={err} weather={weather} />
+    </div>
+  );
+};
 
 export default App;
